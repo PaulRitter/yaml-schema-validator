6f7d8e98c392a1c1bf7ee180f09b7962ae4dd64f
Paul <ritter.paul1@googlemail.com>
2022-08-14 11:08:16 +0200

    i dont know what i was thinking

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ else
   pip install yamale
 fi
 
-if [ -n "/app/$INPUT_VALIDATORS_REQUIREMENTS" ]; then
-  pip install -r /app/$INPUT_VALIDATORS_REQUIREMENTS
+if [ -n "$INPUT_VALIDATORS_REQUIREMENTS" ]; then
+  pip install -r $INPUT_VALIDATORS_REQUIREMENTS
 fi
 
 python3 /app/validate.py
